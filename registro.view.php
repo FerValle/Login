1b46967bd6c7addd03229eb1f87cbb77d77bd093
FerValle <fervalle264@gmail.com>
2017-09-19 21:00:25 -0300

    Cambio Nombres archivos y add Registro
    - Se cambio el nombre del archivo del index y su vista por contenido.view y contenido.php
    - Se cambio el titulo Contenido por Bienvenido
    - Se creo el archivo registro, donde el usuario completa formulario de registro.

---
+++ registro.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Registrate</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link href="css/login.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="contendor">
+            <h1 class="titulo">Registrate</h1>
+            <hr class = 'border'>
+      
+        </div>
+
+    
+    </body>
+</html>
